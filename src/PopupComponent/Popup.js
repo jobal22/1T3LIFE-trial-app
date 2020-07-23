7072afeb7640f0c9f30dc41b3656d61862e4bdf4
Jobal <jbaldwije@gmail.com>
2020-07-23 00:17:11 -0400

    style

--- src/PopupComponent/Popup.js
+++ src/PopupComponent/Popup.js
@@ -12,7 +12,7 @@ export default class PopUp extends Component {
           </header>
           <main className='MainPU'>
             <p className='PUP'><b>1T3 Life</b>: Aiming for the glory of God through the grace of the gospel by practicing living a 1 Timothy 3 life in the home, in the church and in the world.</p>
-            <p className='PUP'>Resources: All the handouts on 1T3Life.org are free and are intended for encouragement and educational purposes.</p>
+            <p className='PUP'><b>Resources</b>: All the handouts on 1T3Life.org are free and are intended for encouragement and educational purposes.</p>
           </main>
             <button className='ClosePU' onClick={this.props.closePopup}>Next</button>
         </div>
