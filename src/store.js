99830a67416080c504b9dbe670cfcf4f2470bb8b
Jobal <jbaldwije@gmail.com>
2020-07-23 17:29:57 -0400

    added files and jpg

--- src/store.js
+++ src/store.js
@@ -11,6 +11,7 @@ const RTlogo = require('./images/RTLogo3.png');
 const ARlogo = require('./images/ARLogo2.png');
 const RClogo = require('./images/RCLogo.png');
 const RDlogo = require('./images/RDLogo.png');
+
 const goodnews1 = require('./images/GoodNews1.jpg');
 const goodnews2 = require('./images/GoodNews2.jpg');
 const goodnews3 = require('./images/GoodNews3.jpg');
@@ -19,12 +20,61 @@ const growinginstructions2 = require('./images/GrowingInstructions2.jpg');
 const growinginstructions3 = require('./images/GrowingInstructions3.jpg');
 const greatcommissionsimplified = require('./images/GreatCommissionSimplified.jpg');
 const christianlife1 = require('./images/ChristianLife1.jpg');
+const tplan1 = require('./images/1T3Plan1.jpg');
+const tplan2 = require('./images/1T3Plan2.jpg');
+const tplan3 = require('./images/1T3Plan3.jpg');
+const additionalresources1 = require('./images/AdditionalResources1.jpg');
+const additionalresources2 = require('./images/AdditionalResources2.jpg');
+const additionalresources3 = require('./images/AdditionalResources3.jpg');
+const additionalresources4 = require('./images/AdditionalResources4.jpg');
+const additionalresources5 = require('./images/AdditionalResources5.jpg');
+const additionalresources6 = require('./images/AdditionalResources6.jpg');
+const additionalresources7 = require('./images/AdditionalResources7.jpg');
+const additionalresources8 = require('./images/AdditionalResources8.jpg');
+const additionalresources9 = require('./images/AdditionalResources9.jpg');
+const additionalresources10 = require('./images/AdditionalResources10.jpg');
+const additionalresources11 = require('./images/AdditionalResources11.jpg');
+const greatcommissiontheology = require('./images/GreatCommissionTheology.jpg');
+const reasonabledesign1 = require('./images/ReasonableDesign-01.jpg');
+const reasonabledesign2 = require('./images/ReasonableDesign-02.jpg');
+const reasonabledesign3 = require('./images/ReasonableDesign-03.jpg');
+const reasonabledesign4 = require('./images/ReasonableDesign-04.jpg');
+const reasonabledesign5 = require('./images/ReasonableDesign-05.jpg');
+const reasonabledesign6 = require('./images/ReasonableDesign-06.jpg');
+const reasonabledesign7 = require('./images/ReasonableDesign-07.jpg');
+const reasonabledesign8 = require('./images/ReasonableDesign-08.jpg');
+const reasonabledesign9 = require('./images/ReasonableDesign-09.jpg');
+const reasonabledesign10 = require('./images/ReasonableDesign-10.jpg');
+const reasonabledesign11 = require('./images/ReasonableDesign-11.jpg');
+const reasonabledesign12 = require('./images/ReasonableDesign-12.jpg');
+const reasonabledesign13 = require('./images/ReasonableDesign-13.jpg');
+const reasonabledesign14 = require('./images/ReasonableDesign-14.jpg');
+const reasonabledesign15 = require('./images/ReasonableDesign-15.jpg');
+const reasonabletrust1 = require('./images/ReasonableTrust-01.jpg');
+const reasonabletrust2 = require('./images/ReasonableTrust-02.jpg');
+const reasonabletrust3 = require('./images/ReasonableTrust-03.jpg');
+const reasonabletrust4 = require('./images/ReasonableTrust-04.jpg');
+const reasonabletrust5 = require('./images/ReasonableTrust-05.jpg');
+const reasonabletrust6 = require('./images/ReasonableTrust-06.jpg');
+const reasonabletrust7 = require('./images/ReasonableTrust-07.jpg');
+const reasonabletrust8 = require('./images/ReasonableTrust-08.jpg');
+const reasonabletrust9 = require('./images/ReasonableTrust-09.jpg');
+const reasonabletrust10 = require('./images/ReasonableTrust-10.jpg');
+const resolvingconflict1 = require('./images/ResolvingConflict-1.jpg');
+const resolvingconflict2 = require('./images/ResolvingConflict-2.jpg');
+const resolvingconflict3 = require('./images/ResolvingConflict-3.jpg');
+
+
 const goodnewspdf = require('./Files/GoodNews.pdf');
 const growinginstructionspdf = require('./Files/GrowingInstructions.pdf');
 const christianlifepdf = require('./Files/ChristianLife.pdf');
 const greatcommissionsimplifiedpdf = require('./Files/GreatCommissionSimplified.pdf');
-
-
+const tplanpdf = require('./Files/1T3Plan.pdf');
+const additionalresourcespdf = require('./Files/AdditionalResources.pdf');
+const greatcommissiontheologypdf = require('./Files/GreatCommissionTheology.pdf');
+const reasonabledesignpdf = require('./Files/ReasonableDesign.pdf');
+const reasonabletrustpdf = require('./Files/ReasonableTrust.pdf');
+const resolvingconflictpdf = require('./Files/ResolvingConflict.pdf');
 
 export default {
   "files": [
@@ -66,15 +116,15 @@ export default {
     {
       "id": "6",
       "name": "1T3 Plan",
-      "content": 'N/A',
-      "images": 'N/A',
+      "content": tplanpdf,
+      "images": [tplan1, tplan2, tplan3],
       "logo": Plogo
     },
     {
       "id": "7",
       "name": "A Theology of the Great Commission",
-      "content": 'N/A',
-      "images": 'N/A',
+      "content": greatcommissiontheologypdf,
+      "images": [greatcommissiontheology],
       "logo": GCTlogo
     },
     {
@@ -87,29 +137,29 @@ export default {
     {
       "id": "9",
       "name": "Resolving Conflict",
-      "content": 'N/A',
-      "images": 'N/A',
+      "content": resolvingconflictpdf,
+      "images": [resolvingconflict1, resolvingconflict2, resolvingconflict3],
       "logo": RClogo
     },
     {
       "id": "10",
       "name": "Reasonable Trust",
-      "content": 'N/A',
-      "images": 'N/A',
+      "content": reasonabletrustpdf,
+      "images": [reasonabletrust1, reasonabletrust2, reasonabletrust3, reasonabletrust4, reasonabletrust5, reasonabletrust6, reasonabletrust7, reasonabletrust8, reasonabletrust9, reasonabletrust10],
       "logo": RTlogo
     },
     {
       "id": "11",
       "name": "Reasonable Design",
-      "content": 'N/A',
-      "images": 'N/A',
+      "content": reasonabledesignpdf,
+      "images": [reasonabledesign1, reasonabledesign2, reasonabledesign3, reasonabledesign4, reasonabledesign5, reasonabledesign6, reasonabledesign7, reasonabledesign8, reasonabledesign9, reasonabledesign10, reasonabledesign11, reasonabledesign12, reasonabledesign13, reasonabledesign14, reasonabledesign15],
       "logo": RDlogo
     },
     {
       "id": "12",
       "name": "Additional Resources",
-      "content": 'N/A',
-      "images": 'N/A',
+      "content": additionalresourcespdf,
+      "images": [additionalresources1, additionalresources2, additionalresources3, additionalresources4, additionalresources5, additionalresources6, additionalresources7, additionalresources8, additionalresources9, additionalresources10, additionalresources11],
       "logo": ARlogo
     },
   ]
