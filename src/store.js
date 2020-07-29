21bb8349090c8e26e7e219a3acaf9dcb0c7586c4
Jobal <jbaldwije@gmail.com>
2020-07-29 14:49:22 -0400

    add pdf and img

--- src/store.js
+++ src/store.js
@@ -64,6 +64,118 @@ const reasonabletrust10 = require('./images/ReasonableTrust-10.jpg');
 const resolvingconflict1 = require('./images/ResolvingConflict-1.jpg');
 const resolvingconflict2 = require('./images/ResolvingConflict-2.jpg');
 const resolvingconflict3 = require('./images/ResolvingConflict-3.jpg');
+const guideforlife1 = require('./images/AGuideforLife-01.jpg');
+const guideforlife2 = require('./images/AGuideforLife-02.jpg');
+const guideforlife3 = require('./images/AGuideforLife-03.jpg');
+const guideforlife4 = require('./images/AGuideforLife-04.jpg');
+const guideforlife5 = require('./images/AGuideforLife-05.jpg');
+const guideforlife6 = require('./images/AGuideforLife-06.jpg');
+const guideforlife7 = require('./images/AGuideforLife-07.jpg');
+const guideforlife8 = require('./images/AGuideforLife-08.jpg');
+const guideforlife9 = require('./images/AGuideforLife-09.jpg');
+const guideforlife10 = require('./images/AGuideforLife-10.jpg');
+const guideforlife11 = require('./images/AGuideforLife-11.jpg');
+const guideforlife12 = require('./images/AGuideforLife-12.jpg');
+const guideforlife13 = require('./images/AGuideforLife-13.jpg');
+const guideforlife14 = require('./images/AGuideforLife-14.jpg');
+const guideforlife15 = require('./images/AGuideforLife-15.jpg');
+const guideforlife16 = require('./images/AGuideforLife-16.jpg');
+const guideforlife17 = require('./images/AGuideforLife-17.jpg');
+const guideforlife18 = require('./images/AGuideforLife-18.jpg');
+const guideforlife19 = require('./images/AGuideforLife-19.jpg');
+const guideforlife20 = require('./images/AGuideforLife-20.jpg');
+const guideforlife21 = require('./images/AGuideforLife-21.jpg');
+const guideforlife22 = require('./images/AGuideforLife-22.jpg');
+const guideforlife23 = require('./images/AGuideforLife-23.jpg');
+const guideforlife24 = require('./images/AGuideforLife-24.jpg');
+const guideforlife25 = require('./images/AGuideforLife-25.jpg');
+const guideforlife26 = require('./images/AGuideforLife-26.jpg');
+const guideforlife27 = require('./images/AGuideforLife-27.jpg');
+const guideforlife28 = require('./images/AGuideforLife-28.jpg');
+const guideforlife29 = require('./images/AGuideforLife-29.jpg');
+const guideforlife30 = require('./images/AGuideforLife-30.jpg');
+const guideforlife31 = require('./images/AGuideforLife-31.jpg');
+const guideforlife32 = require('./images/AGuideforLife-32.jpg');
+const guideforlife33 = require('./images/AGuideforLife-33.jpg');
+const guideforlife34 = require('./images/AGuideforLife-34.jpg');
+const guideforlife35 = require('./images/AGuideforLife-35.jpg');
+const guideforlife36 = require('./images/AGuideforLife-36.jpg');
+const guideforlife37 = require('./images/AGuideforLife-37.jpg');
+const guideforlife38 = require('./images/AGuideforLife-38.jpg');
+const guideforlife39 = require('./images/AGuideforLife-39.jpg');
+const guideforlife40 = require('./images/AGuideforLife-40.jpg');
+const guideforlife41 = require('./images/AGuideforLife-41.jpg');
+const guideforlife42 = require('./images/AGuideforLife-42.jpg');
+const guideforlife43 = require('./images/AGuideforLife-43.jpg');
+const guideforlife44 = require('./images/AGuideforLife-44.jpg');
+const guideforlife45 = require('./images/AGuideforLife-45.jpg');
+const guideforlife46 = require('./images/AGuideforLife-46.jpg');
+const guideforlife47 = require('./images/AGuideforLife-47.jpg');
+const guideforlife48 = require('./images/AGuideforLife-48.jpg');
+const guideforlife49 = require('./images/AGuideforLife-49.jpg');
+const guideforlife50 = require('./images/AGuideforLife-50.jpg');
+const guideforlife51 = require('./images/AGuideforLife-51.jpg');
+const guideforlife52 = require('./images/AGuideforLife-52.jpg');
+const guideforlife53 = require('./images/AGuideforLife-53.jpg');
+const guideforlife54 = require('./images/AGuideforLife-54.jpg');
+const guideforlife55 = require('./images/AGuideforLife-55.jpg');
+const guideforlife56 = require('./images/AGuideforLife-56.jpg');
+const guideforlife57 = require('./images/AGuideforLife-57.jpg');
+const guideforlife58 = require('./images/AGuideforLife-58.jpg');
+const guideforlife59 = require('./images/AGuideforLife-59.jpg');
+const guideforlife60 = require('./images/AGuideforLife-60.jpg');
+const guideforlife61 = require('./images/AGuideforLife-61.jpg');
+const guideforlife62 = require('./images/AGuideforLife-62.jpg');
+const guideforlife63 = require('./images/AGuideforLife-63.jpg');
+const guideforlife64 = require('./images/AGuideforLife-64.jpg');
+const guideforlife65 = require('./images/AGuideforLife-65.jpg');
+const guideforlife66 = require('./images/AGuideforLife-66.jpg');
+const guideforlife67 = require('./images/AGuideforLife-67.jpg');
+const guideforlife68 = require('./images/AGuideforLife-68.jpg');
+const guideforlife69 = require('./images/AGuideforLife-69.jpg');
+const guideforlife70 = require('./images/AGuideforLife-70.jpg');
+const sharinggoodnews1 = require('./images/SharingTheGoodNews-01.jpg');
+const sharinggoodnews2 = require('./images/SharingTheGoodNews-02.jpg');
+const sharinggoodnews3 = require('./images/SharingTheGoodNews-03.jpg');
+const sharinggoodnews4 = require('./images/SharingTheGoodNews-04.jpg');
+const sharinggoodnews5 = require('./images/SharingTheGoodNews-05.jpg');
+const sharinggoodnews6 = require('./images/SharingTheGoodNews-06.jpg');
+const sharinggoodnews7 = require('./images/SharingTheGoodNews-07.jpg');
+const sharinggoodnews8 = require('./images/SharingTheGoodNews-08.jpg');
+const sharinggoodnews9 = require('./images/SharingTheGoodNews-09.jpg');
+const sharinggoodnews10 = require('./images/SharingTheGoodNews-10.jpg');
+const sharinggoodnews11 = require('./images/SharingTheGoodNews-11.jpg');
+const sharinggoodnews12 = require('./images/SharingTheGoodNews-12.jpg');
+const sharinggoodnews13 = require('./images/SharingTheGoodNews-13.jpg');
+const sharinggoodnews14 = require('./images/SharingTheGoodNews-14.jpg');
+const sharinggoodnews15 = require('./images/SharingTheGoodNews-15.jpg');
+const sharinggoodnews16 = require('./images/SharingTheGoodNews-16.jpg');
+const sharinggoodnews17 = require('./images/SharingTheGoodNews-17.jpg');
+const sharinggoodnews18 = require('./images/SharingTheGoodNews-18.jpg');
+const sharinggoodnews19 = require('./images/SharingTheGoodNews-19.jpg');
+const sharinggoodnews20 = require('./images/SharingTheGoodNews-20.jpg');
+const sharinggoodnews21 = require('./images/SharingTheGoodNews-21.jpg');
+const sharinggoodnews22 = require('./images/SharingTheGoodNews-22.jpg');
+const sharinggoodnews23 = require('./images/SharingTheGoodNews-23.jpg');
+const sharinggoodnews24 = require('./images/SharingTheGoodNews-24.jpg');
+const sharinggoodnews25 = require('./images/SharingTheGoodNews-25.jpg');
+const sharinggoodnews26 = require('./images/SharingTheGoodNews-26.jpg');
+const sharinggoodnews27 = require('./images/SharingTheGoodNews-27.jpg');
+const sharinggoodnews28 = require('./images/SharingTheGoodNews-28.jpg');
+const sharinggoodnews29 = require('./images/SharingTheGoodNews-29.jpg');
+const sharinggoodnews30 = require('./images/SharingTheGoodNews-30.jpg');
+const sharinggoodnews31 = require('./images/SharingTheGoodNews-31.jpg');
+const sharinggoodnews32 = require('./images/SharingTheGoodNews-32.jpg');
+const sharinggoodnews33 = require('./images/SharingTheGoodNews-33.jpg');
+const sharinggoodnews34 = require('./images/SharingTheGoodNews-34.jpg');
+const sharinggoodnews35 = require('./images/SharingTheGoodNews-35.jpg');
+const sharinggoodnews36 = require('./images/SharingTheGoodNews-36.jpg');
+const sharinggoodnews37 = require('./images/SharingTheGoodNews-37.jpg');
+const sharinggoodnews38 = require('./images/SharingTheGoodNews-38.jpg');
+const sharinggoodnews39 = require('./images/SharingTheGoodNews-39.jpg');
+const sharinggoodnews40 = require('./images/SharingTheGoodNews-40.jpg');
+const sharinggoodnews41 = require('./images/SharingTheGoodNews-41.jpg');
+
 
 const goodnewspdf = require('./Files/GoodNews.pdf');
 const growinginstructionspdf = require('./Files/GrowingInstructions.pdf');
@@ -75,6 +187,9 @@ const greatcommissiontheologypdf = require('./Files/GreatCommissionTheology.pdf'
 const reasonabledesignpdf = require('./Files/ReasonableDesign.pdf');
 const reasonabletrustpdf = require('./Files/ReasonableTrust.pdf');
 const resolvingconflictpdf = require('./Files/ResolvingConflict.pdf');
+const guideforlifepdf = require('./Files/AGuideforLife.pdf');
+const sharingthegoodnewspdf = require('./Files/SharingTheGoodNews.pdf');
+
 
 export default {
   "files": [
@@ -87,9 +202,9 @@ export default {
     },
     {
       "id": "2",
-      "name": "A Way to Share The Good News",
-      "content": 'N/A',
-      "images": [],
+      "name": "Sharing The Good News",
+      "content": sharingthegoodnewspdf,
+      "images": [sharinggoodnews1, sharinggoodnews2, sharinggoodnews3, sharinggoodnews4, sharinggoodnews5, sharinggoodnews6, sharinggoodnews7, sharinggoodnews8, sharinggoodnews9, sharinggoodnews10, sharinggoodnews11, sharinggoodnews12, sharinggoodnews13, sharinggoodnews14, sharinggoodnews15, sharinggoodnews16, sharinggoodnews17, sharinggoodnews18, sharinggoodnews19, sharinggoodnews20, sharinggoodnews21, sharinggoodnews22, sharinggoodnews23, sharinggoodnews24, sharinggoodnews25, sharinggoodnews26, sharinggoodnews27, sharinggoodnews28, sharinggoodnews29, sharinggoodnews30, sharinggoodnews31, sharinggoodnews32, sharinggoodnews33, sharinggoodnews34, sharinggoodnews35, sharinggoodnews36, sharinggoodnews37, sharinggoodnews38, sharinggoodnews39, sharinggoodnews40, sharinggoodnews41],
       "logo": Slogo
     },
     {
@@ -109,8 +224,8 @@ export default {
     {
       "id": "5",
       "name": "A Guide for Life",
-      "content": 'N/A',
-      "images": [],
+      "content": guideforlifepdf,
+      "images": [guideforlife1, guideforlife2, guideforlife3, guideforlife4, guideforlife5, guideforlife6, guideforlife7, guideforlife8, guideforlife9, guideforlife10, guideforlife11, guideforlife12, guideforlife13, guideforlife14, guideforlife15, guideforlife16, guideforlife17, guideforlife18, guideforlife19, guideforlife20, guideforlife21, guideforlife22, guideforlife23, guideforlife24, guideforlife25, guideforlife26, guideforlife27, guideforlife28, guideforlife29, guideforlife30, guideforlife31, guideforlife32, guideforlife33, guideforlife34, guideforlife35, guideforlife36, guideforlife37, guideforlife38, guideforlife39, guideforlife40, guideforlife41, guideforlife42, guideforlife43, guideforlife44, guideforlife45, guideforlife46, guideforlife47, guideforlife48, guideforlife49, guideforlife50, guideforlife51, guideforlife52, guideforlife53, guideforlife54, guideforlife55, guideforlife56, guideforlife57, guideforlife58, guideforlife59, guideforlife60, guideforlife61, guideforlife62, guideforlife63, guideforlife64, guideforlife65, guideforlife66, guideforlife67, guideforlife68, guideforlife69, guideforlife70],
       "logo": GLlogo
     },
     {
